1d51c9f1b272de6af731bf83b6d26a5132e6f80e
Xing Tao Shi <xshi603@gmail.com>
2020-01-18 15:12:51 -0500

    added userhomepage

--- src/components/containers/sessionCard.js
+++ src/components/containers/sessionCard.js
@@ -5,36 +5,7 @@ class SessionCard extends Component {
     super(props);
   }
   render() {
-    let climb1 = {
-      grade: "V3",
-      name: "",
-      color: "",
-      attempts: 3,
-      sends: 2,
-      notes: "Lots of crimpy holds",
-      imageUrl: ""
-    };
-    let climb2 = {
-      grade: "V4",
-      name: "",
-      color: "",
-      attempts: 3,
-      sends: 2,
-      notes: "Lots of crimpy holds",
-      imageUrl: ""
-    };
-    let climbs = [climb1, climb2];
-    let parsed = climbs.map(climb => {
-      return (
-        <div>
-          <p> Problem Grade: {climb.grade} </p>
-          <p> Attempts: {climb.attempts} </p>
-          <p> Sends: {climb.sends} </p>
-          <p> Notes: {climb.notes} </p> <br />
-        </div>
-      );
-    });
-    return parsed;
+    return "sessionCard is here";
   }
 }
 
