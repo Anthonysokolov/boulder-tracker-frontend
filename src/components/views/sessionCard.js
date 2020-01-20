c7a053e08299080f7c352506d88e41fd94b6ea21
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-01-20 17:23:21 -0500

    stylized cards

--- src/components/views/sessionCard.js
+++ src/components/views/sessionCard.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import "./userHomePage.css";
+import "../../styles/card.css";
+import "../../styles/common.css";
 
 export default function SessionCard(props) {
   return (
     <div>
-      <Link to={"/session/" + props.id}>
-        <div className="boxed">
-          <div className="column">
-            <p> Date: {props.date} </p>
-          </div>
-          <div>
-            <p> Location: {props.location} </p>
-            <p> Comments: {props.comments} </p>
+      <Link to={"/session/" + props.id} className="no-underline">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title"> {(new Date(props.date)).toLocaleDateString()} </h5>
+            <h6 className="card-subtitle"> {props.location} </h6>
+            <p className="card-text"> Session #{props.index}: {props.comments} </p>
           </div>
         </div>
       </Link>
