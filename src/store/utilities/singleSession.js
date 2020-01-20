fcdcbb47daad0c2663a013579337a3659dfef0a4
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-01-20 12:23:29 -0500

    setup actions to select a single session

--- src/store/utilities/singleSession.js
+++ src/store/utilities/singleSession.js
@@ -2,17 +2,45 @@ import { promiseTimeout } from "./index.js";
 
 /********************************* ACTIONS ***********************************/
 
-/********************************* THUNKS ***********************************/
-
+const SELECT_SESSION = "SELECT_SESSION";
 
 /**
- * Just a template function, will be replaced by something 
+ * Expects an object with the following shape:
+ *		string location, string date, string comments, boolean exists, int id,
+ *		array climbs[]
 */
-export function fetchSessionsThunk() {
+function selectAction(data) {
+	return {
+		type: SELECT_SESSION,
+		payload: data
+	};
+}
+
+/********************************* THUNKS ***********************************/
+
+const hardcodedSession = {location: "Mountains R Us", date: "18-01-2020 18:54:00", comments: "went out with friends", climbs: [], id: 0};
+const nonexistantSession = {location: "", date: "", comments: "", climbs: [], id: 1, exists: false};
+
+/**
+ * Will fetch detailed information about a session from the API.
+ * @pre       the session should exist
+ * @param id  the numerical ID of the requested session. 
+ * @post      the selectAction() creator will be called with the data from
+ *            response if the operation was succesful. Otherwise, it will
+ *            be given an object with the expected shape but empty fields
+ *            and the exists flag will be false on the object.
+*/
+export function fetchSessionsThunk(id) {
 	return function(dispatch) {
-		promiseTimeout(1000).then(function() {
-			//dispatch(some action);
+		promiseTimeout(1000)
+		.then(function() {
+			//this will be the data received from the response
+			hardcodedSession.exists = true;
+			dispatch(selectAction(hardcodedSession));
 		})
+		.catch(function() {
+			dispatch(selectAction(nonexistantSession))
+		});
 	}
 }
 
@@ -21,6 +49,8 @@ const initialState = {};
 
 export default function singleSessionReducer(state = initialState, action) {
 	switch(action.type) {
+		case SELECT_SESSION:
+			return Object.assign({}, state, payload);
 		default:
 			return state;
 	}
