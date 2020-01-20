f2a405e609225dcc4e4a4e6aefdf86c4028b9ba2
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-01-20 15:02:27 -0500

    converted class to function

--- src/components/views/sessionPage.js
+++ src/components/views/sessionPage.js
@@ -1,37 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import Navbar from "./../containers/navbar";
 import ProblemCard from "./../containers/problemCard";
 
-class SessionPage extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    let climbs = [
-      { grade: "V3", name: "", color: "", attempts: "", send: "", notes: "" }
-    ];
-    let data = {
-      date: "06/19/20",
-      duration: 90,
-      location: "Brooklyn Boulders"
-    };
-    // IDEAL FUNCTIONALITY
-    // Option to view stats
-    return (
-      <div>
-        <Navbar />
-        <div className="session">
-          <p> Session Date: {data.date} </p>
-          <p> Location: {data.location} </p>
-          <p> Duration: {data.duration} minutes</p>
-          <p> Ascents: </p>
-          <p> Top Grade: </p> <br />
-          <h3> List of Climbs: </h3>
-          <div className="climbs"></div>
+function SessionPage(props) {
+  return (
+    <div>
+      <div className="session">
+        <h1>{props.date}</h1>
+        <p>{props.location}</p>
+        <div>
+          <h6>Comments:</h6>
+          <p>{props.comments}</p>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default SessionPage;
