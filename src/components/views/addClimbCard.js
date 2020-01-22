b84bae3c3858f15157b2419b08312d463b8fc583
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-01-22 04:48:00 -0500

    restyled the add climb component

--- src/components/views/addClimbCard.js
+++ src/components/views/addClimbCard.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { FormBase, FormField, FormRow } from "./Form.jsx";
 
-import "../../styles/card.css";
 import "../../styles/common.css";
 
 export default function AddClimbCard(props) {
+  let grades = [];
+  for(let i = 0; i <= 15; i++) grades.push("V" + i);
   return (
     <div>
-      <div className="card">
-        Name: <input type="text" onChange={props.nameHandler} /> <br />
-        Grade: <input type="text" onChange={props.gradeHandler} /> <br />
-        Comments: <input type="text" onChange={props.commentHandler} /> <br />
-        <input type="submit" onClick={props.submitHandler} />
-      </div>
+      <FormBase button="Add" title="Add Problem" onSubmit={props.submitHandler}>
+        <FormField type="text" name="name" handler={props.nameHandler} label="Nickname for this problem" />
+        <FormField type="select" name="grade" handler={props.gradeHandler} label="Difficulty of the problem (0 is easiest and 15 is the hardest)" options={grades} />
+        <FormRow>
+          <FormField type="number" name="attempts" handler={props.attemptsHandler} label="# Attempts" />
+          <FormField type="number" name="sends" handler={props.sendsHandler} label="# Sends (succesful ascents)" />
+        </FormRow>
+        <FormField type="textarea" name="comments" handler={props.commentHandler} label="Notes" />
+      </FormBase>
     </div>
   );
 }
