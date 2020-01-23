c765608237a96c6260e8f4aa757992f3ae2429e0
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-01-23 02:25:34 -0500

    changed format of thunk to make debugging easier

--- src/store/utilities/user.js
+++ src/store/utilities/user.js
@@ -1,9 +1,11 @@
-import { promiseTimeout } from "./index.js";
-const axios = require('axios')
+import axios from "axios";
+import * as StatusCode from "./index.js";
 
 /********************************* ACTIONS ***********************************/
+
 const GET_USER = "GET_USER";
 const REMOVE_USER = "REMOVE_USER";
+const SET_ERROR = "SET_ERROR";
 
 // ACTION CREATORS
 const getUser = user => {
@@ -13,33 +15,39 @@ const getUser = user => {
   }
 }
 
+/**
+ * Makes an action that will update the status code and message of this slice
+ * of the Redux store. Please use it to indicate the progress of some process.
+ * Call it before and after an asynchronous action. This will let subscribed
+ * components change their state when the status is right.
+ * @param statusCode  a constant from StatusCode (index.js)
+ * @param message     a string to show the user 
+*/
+function setStatus(statusCode, message) {
+  return {
+    type: SET_ERROR,
+    status: statusCode,
+    message: message
+  };
+}
+
 /********************************* THUNKS ***********************************/
 
 /**
  * Just a template function, will be replaced by something soon.
 */
-export function loginThunk() {
-	return function(dispatch) {
-		promiseTimeout(1000).then(function() {
-			//dispatch(some action here);
-		})
-	}
-}
-
-export const auth = (email, password, method) => async dispatch => {
-  let res;
-  try {
-    res = await axios.post(`/auth/${method}`, { username:email, password }, { withCredentials: true });
-  }
-  catch (authError) {
-    return dispatch(getUser({ error: authError }));
-  }
-
-  try {
-    dispatch(getUser(res.data));
-  }
-  catch (dispatchOrHistoryErr) {
-    console.error(dispatchOrHistoryErr);
+export function loginThunk(email, password) {
+  return async dispatch => {
+    dispatch(setStatus(StatusCode.LOADING, "Logging in..."));
+    let res;
+    try {
+      res = await axios.post("/auth/login", { username:email, password }, { withCredentials: true });
+      console.log("Response from logging in: ", res);
+      dispatch(setStatus(StatusCode.SUCCESS, "Welcome Back!"));
+    }
+    catch (authError) {
+      dispatch(setStatus(StatusCode.ERROR, "Wrong username or password."));
+    }
   }
 }
 
