5d0d4c4f37c1152e123aa678f9451704ed6047d1
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-01-23 17:15:47 -0500

    using protected routes to ensure login

--- src/App.js
+++ src/App.js
@@ -9,6 +9,31 @@ import AddSessionPage from "./components/containers/addSessionPage";
 import {Login, Signup} from './components/containers/authFormContainer';
 import Navbar from "./components/containers/navbar.js";
 
+const ProtectedRoute = ({ component: Comp, loggedIn, path, ...rest }) => {
+  return (
+    <Route
+      path={path}
+      {...rest}
+      render={props => {
+        return loggedIn ? (
+          <Comp {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: {
+                prevLocation: path,
+                loginStatus: "error",
+                loginMessage: "You need to login first!",
+              },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
 function App(props) {
   const HomeComponent = () => <Home />;
   const UserHomeComponent = () => <UserHomePage />;
@@ -16,20 +41,13 @@ function App(props) {
   return (
     <div className="app">
       <Switch>
-        {props.isLoggedIn && (
-          <Switch>
-            <Navbar />
-            {/* Routes placed here will only be available if user is logged in */}
-            <Route exact path="/session/add" component={AddSessionPage} />
-            <Route path="/session/:id" component={SingleSessionPage} />
-            <Route exact path="/home" render={UserHomeComponent} />
-          </Switch>
-        )}
+        <ProtectedRoute path="/home" component={UserHomePage} loggedIn={props.isLoggedIn} />
+        <ProtectedRoute exact path="/session/add" component={AddSessionPage} />
+        <ProtectedRoute path="/session/:id" component={SingleSessionPage} />
         <Route exact path="/" render={HomeComponent} />
-        <Route exact path='/login'>
-          {props.isLoggedIn? <Redirect to="/home" /> : <Login />}
-        </Route>
+        <Route exact path='/login' component={Login} />
         <Route exact path='/signup' component={Signup}/>
+        <Route render={() => <h1>Unknown location</h1>} />
       </Switch>
     </div>
   );
