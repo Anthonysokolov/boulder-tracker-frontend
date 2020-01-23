f120d7886c4c6b5380761d80794e8eae3dc9c695
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-01-23 11:54:41 -0500

    restructured routes to require login

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Provider } from "react-redux";
+import { withRouter , Switch, Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 
-import store from "./store";
-import "./App.css";
 import Home from "./components/views/home";
 import SingleSessionPage from "./components/containers/singleSessionPage";
 import UserHomePage from "./components/containers/userHomePage";
 import AddSessionPage from "./components/containers/addSessionPage";
-import {Login, Signup} from './components/containers/authFormContainer'
+import {Login, Signup} from './components/containers/authFormContainer';
+import Navbar from "./components/containers/navbar.js";
 
-function App() {
+function App(props) {
   const HomeComponent = () => <Home />;
   const UserHomeComponent = () => <UserHomePage />;
 
   return (
-    <Provider store={store}>
-      <Router>
-        <Switch>
-          <Route exact path="/session/add" component={AddSessionPage} />
-          <Route path="/session/:id" component={SingleSessionPage} />
-          <Route exact path="/home" render={UserHomeComponent} />
-          <Route exact path="/" render={HomeComponent} />
-          <Route exact path='/login' component={Login}/>
-          <Route exact path='/signup' component={Signup}/>
-        </Switch>
-      </Router>
-    </Provider>
+    <div className="app">
+      <Switch>
+        {props.isLoggedIn && (
+          <Switch>
+            <Navbar />
+            {/* Routes placed here will only be available if user is logged in */}
+            <Route exact path="/session/add" component={AddSessionPage} />
+            <Route path="/session/:id" component={SingleSessionPage} />
+            <Route exact path="/home" render={UserHomeComponent} />
+          </Switch>
+        )}
+        <Route exact path="/" render={HomeComponent} />
+        <Route exact path='/login'>
+          {props.isLoggedIn? <Redirect to="/home" /> : <Login />}
+        </Route>
+        <Route exact path='/signup' component={Signup}/>
+      </Switch>
+    </div>
   );
 }
 
-export default App;
+function mapUserToProps(appState) {
+  return {
+    isLoggedIn: appState.user.isLoggedIn
+  };
+}
+
+export default withRouter(connect(mapUserToProps)(App));
