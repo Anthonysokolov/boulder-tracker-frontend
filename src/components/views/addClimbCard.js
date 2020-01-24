3fbdd3ae73fc0492d020ec2b5503570b3ed35c54
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-01-24 02:00:50 -0500

    got rid of collapsable feature

--- src/components/views/addClimbCard.js
+++ src/components/views/addClimbCard.js
@@ -9,16 +9,14 @@ export default function AddClimbCard(props) {
   for(let i = 0; i <= 15; i++) grades.push("V" + i);
   return (
     <div>
-      <FormBase button={props.visible? "Add" : false} title="Add Problem" onSubmit={props.submitHandler}>
-        <Collapsible once={false} visible={props.visible}>
-          <FormField type="text" name="name" handler={props.nameHandler} label="Nickname for this problem" />
-          <FormField type="select" name="grade" handler={props.gradeHandler} label="Difficulty of the problem (0 is easiest and 15 is the hardest)" options={grades} />
-          <FormRow>
-            <FormField type="number" name="attempts" handler={props.attemptsHandler} label="# Attempts" />
-            <FormField type="number" name="sends" handler={props.sendsHandler} label="# Sends (succesful ascents)" />
-          </FormRow>
-          <FormField type="textarea" name="comments" handler={props.commentHandler} label="Notes" />
-        </Collapsible>
+      <FormBase button="Add" title="Add Problem" onSubmit={props.submitHandler}>
+        <FormField type="text" name="name" handler={props.nameHandler} label="Nickname for this problem" />
+        <FormField type="select" name="grade" handler={props.gradeHandler} label="Difficulty of the problem (0 is easiest and 15 is the hardest)" options={grades} />
+        <FormRow>
+          <FormField type="number" name="attempts" handler={props.attemptsHandler} label="# Attempts" />
+          <FormField type="number" name="sends" handler={props.sendsHandler} label="# Sends (succesful ascents)" />
+        </FormRow>
+        <FormField type="textarea" name="comments" handler={props.commentHandler} label="Notes" />
       </FormBase>
     </div>
   );
